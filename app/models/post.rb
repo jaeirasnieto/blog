2fc98de71c848dbfa134b183e979c6377dd3cce0
jaeiras <jaeirasnieto@gmail.com>
2013-11-03 00:02:27 +0100

    subida 1

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,4 @@ class Post < ActiveRecord::Base
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
-# esto es un comentario de prueba
+# esto es un comentario de prueba 1
